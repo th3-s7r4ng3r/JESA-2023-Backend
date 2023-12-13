02cc74f1e0d750f5e5bd3ad242e5aceb7fba45af
VinuraGallage <th3-s7r4ng3r@users.noreply.github.com>
2023-12-13 14:12:34 +0530

    Ref: Change port number to deploy in the server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 // Server configuration
-const PORT = 8080;
+const PORT = process.env.PORT || 9090;
 const attendeesFile = path.join(__dirname, "data", "attendees.json");
 const apiKeysFile = path.join(__dirname, "data", "api-keys.json");
 
