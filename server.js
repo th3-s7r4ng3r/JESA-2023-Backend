55dba17e2e1b6467899708077dddde6ce99e4008
VinuraGallage <th3-s7r4ng3r@users.noreply.github.com>
2023-12-15 00:27:39 +0530

    Ref: Updated server port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 // Server configuration
-// const PORT = 443;
+const PORT = 8080;
 const attendeesFile = path.join(__dirname, "data", "attendees.json");
 const apiKeysFile = path.join(__dirname, "data", "api-keys.json");
 
